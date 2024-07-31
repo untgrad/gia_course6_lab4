0889e975b7722c3c4ea0e8baca5e74a8a06c26f8
untgrad <66182266+untgrad@users.noreply.github.com>
2024-07-31 18:34:06 -0500

    Update reports.py

--- reports.py
+++ reports.py
@@ -8,7 +8,7 @@ from reportlab.lib import colors
 def generate_report(attachment, title, paragraph):
     styles = getSampleStyleSheet()
     report = SimpleDocTemplate(attachment)
-    report_title = Paragraph(title, styles["h1"])
-    report_info = Paragraph(paragraph, styles["BodyText"])
+    report_title = Paragraph(title, styles['h1'])
+    report_info = Paragraph(paragraph, styles['BodyText'])
     empty_line = Spacer(1, 20)
     report.build([report_title, empty_line, report_info])
