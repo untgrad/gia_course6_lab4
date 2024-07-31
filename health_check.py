9dd6ad67679065f727776fad57c6e33b01335bc5
untgrad <66182266+untgrad@users.noreply.github.com>
2024-07-31 18:31:12 -0500

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -7,7 +7,7 @@ import emails
 max_cpu_usage_perc = 80
 max_disk_avail_perc = 20
 max_mem_avail_mb = 500
-chk_local_host_ip = "127.0.0.1"
+chk_local_host_ip = '127.0.0.1'
 
 
 def chkCPU():
@@ -19,7 +19,7 @@ def chkCPU():
 def chkDisk():
     """check if Disk usage exceeds max threshold"""
     max_disk_usage_perc = 100 - max_disk_avail_perc
-    dsk_usage_perc = psutil.disk_usage("/").percent
+    dsk_usage_perc = psutil.disk_usage('/').percent
     return dsk_usage_perc > max_disk_usage_perc
 
 
@@ -33,24 +33,24 @@ def chkMem():
 
 def chkNet():
     """check if local host name resolves to local IP"""
-    local_host_ip = socket.gethostbyname("localhost")
+    local_host_ip = socket.gethostbyname('localhost')
     return local_host_ip != chk_local_host_ip
 
 
 def sendAlert(alert):
     """output alert error and send email"""
     content = {
-        "sender": "automation@example.com",
-        "receiver": "student@example.com",
-        "subject": alert,
-        "body": "Please check your system and resolve the issue as soon as possible.",
-        "attachment": None,
+        'sender': 'automation@example.com',
+        'receiver': 'student@example.com',
+        'subject': alert,
+        'body': 'Please check your system and resolve the issue as soon as possible.',
+        'attachment': None,
     }
     try:
         message = emails.generate_email(**content)
         emails.send_email(message)
     except:
-        print("unable to send alert email notification!")
+        print('unable to send alert email notification!')
     finally:
         print(alert)
         exit(1)
@@ -58,22 +58,22 @@ def sendAlert(alert):
 
 def main():
     # check system resources:
-    print("checking system resources")
+    print('checking system resources')
     alert = None
     if chkCPU():
-        alert = f"Error - CPU usage is over {max_cpu_usage_perc}%"
+        alert = f'Error - CPU usage is over {max_cpu_usage_perc}%'
     elif chkDisk():
-        alert = f"Error - Available disk space is less than {max_disk_avail_perc}%"
+        alert = f'Error - Available disk space is less than {max_disk_avail_perc}%'
     elif chkMem():
-        alert = f"Error - Available memory is less than {max_mem_avail_mb}MB"
+        alert = f'Error - Available memory is less than {max_mem_avail_mb}MB'
     elif chkNet():
-        alert = f"Error - localhost cannot be resolved to {chk_local_host_ip}"
+        alert = f'Error - localhost cannot be resolved to {chk_local_host_ip}'
 
     # alert if error raised:
     if alert:
         sendAlert(alert)
     else:
-        print("system ok")
+        print('system ok')
 
 
 if __name__ == "__main__":
