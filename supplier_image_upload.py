a8c5c3ff5b0bed20469dd2cfd26eca0b063845b8
untgrad <66182266+untgrad@users.noreply.github.com>
2024-07-31 18:34:56 -0500

    Update supplier_image_upload.py

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -5,18 +5,18 @@ from os import listdir
 # This example shows how a file can be uploaded using
 # The Python Requests module
 
-url = "http://localhost/upload/"
+url = 'http://localhost/upload/'
 
 
 def upload(file, url):
-    with open(file, "rb") as opened:
-        requests.post(url, files={"file": opened})
+    with open(file, 'rb') as opened:
+        requests.post(url, files={'file': opened})
 
 
 # set image dir:
-img_dir = "supplier-data/images/"
+img_dir = 'supplier-data/images/'
 
 # gather list of image files:
-img_files = [img_dir + f for f in listdir(img_dir) if f.endswith(".jpeg")]
+img_files = [img_dir + f for f in listdir(img_dir) if f.endswith('.jpeg')]
 for file in img_files:
     upload(file, url)
