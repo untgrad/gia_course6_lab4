fd0af4f97264fb49c07e2ab0206e13455c78720b
untgrad <66182266+untgrad@users.noreply.github.com>
2024-07-31 18:29:07 -0500

    Update emails.py

--- emails.py
+++ emails.py
@@ -9,18 +9,18 @@ def generate_email(sender, receiver, subject, body, attachment=None):
     """Creates an email with an attachement."""
     # Basic Email formatting
     message = email.message.EmailMessage()
-    message["From"] = sender
-    message["To"] = receiver
-    message["Subject"] = subject
+    message['From'] = sender
+    message['To'] = receiver
+    message['Subject'] = subject
     message.set_content(body)
 
     # Process the attachment and add it to the email
     if attachment:
         attachment_filename = os.path.basename(attachment)
         mime_type, _ = mimetypes.guess_type(attachment)
-        mime_type, mime_subtype = mime_type.split("/", 1)
+        mime_type, mime_subtype = mime_type.split('/', 1)
 
-        with open(attachment, "rb") as ap:
+        with open(attachment, 'rb') as ap:
             message.add_attachment(
                 ap.read(),
                 maintype=mime_type,
@@ -33,6 +33,6 @@ def generate_email(sender, receiver, subject, body, attachment=None):
 
 def send_email(message):
     """Sends the message to the configured SMTP server."""
-    mail_server = smtplib.SMTP("localhost")
+    mail_server = smtplib.SMTP('localhost')
     mail_server.send_message(message)
     mail_server.quit()
