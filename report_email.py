4a4441b0021aa2e379679a6873832cc0e2cdf36a
untgrad <66182266+untgrad@users.noreply.github.com>
2024-07-31 18:33:18 -0500

    Update report_email.py

--- report_email.py
+++ report_email.py
@@ -9,18 +9,18 @@ import emails
 def getDesc(file):
     with open(file) as f:
         lines = f.read().strip().splitlines()
-    name_field = "name: {}".format(lines[0])
-    weight_field = "weight: {}".format(lines[1])
-    return "{}<br/>{}<br/><br/>".format(name_field, weight_field)
+    name_field = 'name: {}'.format(lines[0])
+    weight_field = 'weight: {}'.format(lines[1])
+    return '{}<br/>{}<br/><br/>'.format(name_field, weight_field)
 
 
 def main():
     # set text dir & gather files:
-    txt_dir = "supplier-data/descriptions/"
-    txt_files = [txt_dir + f for f in os.listdir(txt_dir) if f.endswith(".txt")]
+    txt_dir = 'supplier-data/descriptions/'
+    txt_files = [txt_dir + f for f in os.listdir(txt_dir) if f.endswith('.txt')]
 
     # set report file:
-    report_file = "/tmp/processed.pdf"
+    report_file = '/tmp/processed.pdf'
 
     # generate report body:
     report_body = ""
@@ -29,8 +29,8 @@ def main():
 
     # set report title:
     today = datetime.datetime.today()
-    report_title = "Processed Update on {} {}, {}".format(
-        today.strftime("%B"), today.day, today.year
+    report_title = 'Processed Update on {} {}, {}'.format(
+        today.strftime('%B'), today.day, today.year
     )
 
     # generate report file:
@@ -38,15 +38,15 @@ def main():
 
     # generate & send email report:
     content = {
-        "sender": "automation@example.com",
-        "receiver": "{}@example.com".format(os.environ.get("USER")),
-        "subject": "Upload Completed - Online Fruit Store",
-        "body": "All fruits are uploaded to our website successfully. A detailed list is attached to this email.",
-        "attachment": report_file,
+        'sender': 'automation@example.com',
+        'receiver': '{}@example.com'.format(os.environ.get('USER')),
+        'subject': 'Upload Completed - Online Fruit Store',
+        'body': 'All fruits are uploaded to our website successfully. A detailed list is attached to this email.',
+        'attachment': report_file,
     }
     message = emails.generate_email(**content)
     emails.send_email(message)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
